ca36df2b784aa08cc4ca9ff2f5cbb6ae76ed2a20
Supphachoke Suntiwichaya <mrchoke@gmail.com>
2020-09-05 20:21:55 +0700

    lint fix and composition

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueCompositionAPI from '@vue/composition-api'
 
 Vue.config.productionTip = false
+Vue.use(VueCompositionAPI)
 
 new Vue({
   router,
